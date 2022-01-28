62430d88392121e6858c19e3991a6fc86893b5b3
Stephen White <iamsilk3495@gmail.com>
2022-01-28 00:34:06 -0400

    Added /repair and /repairhand commands

---
+++ SilK.Unturned.Essentials/Commands/Items/CRepair.cs
@@ -0,0 +1,52 @@
+﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Prioritization;
+using OpenMod.Core.Commands;
+using OpenMod.Unturned.Items;
+using OpenMod.Unturned.Users;
+using SilK.Unturned.Essentials.Localization;
+using System;
+using System.Linq;
+
+namespace SilK.Unturned.Essentials.Commands.Items
+{
+    [UsedImplicitly]
+    [Command("repair", Priority = Priority.Normal)]
+    [CommandAlias("repairall")]
+    [CommandAlias("fix")]
+    [CommandAlias("fixall")]
+    [CommandDescription("Repairs all items in your inventory.")]
+    [CommandActor(typeof(UnturnedUser))]
+    [LocalizationSection("Items", "Repair")]
+    public class CRepair : EssentialsCommand
+    {
+        public CRepair(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        [UsedImplicitly]
+        protected async UniTask OnExecuteAsync()
+        {
+            await UniTask.SwitchToMainThread();
+
+            var amount = 0;
+
+            foreach (var item in UnturnedPlayer.Inventory.Pages.SelectMany(x => x.Items).OfType<UnturnedInventoryItem>())
+            {
+                var maxQuality = item.Item.Asset.MaxQuality;
+
+                if (!maxQuality.HasValue)
+                {
+                    continue;
+                }
+
+                await item.Item.SetItemQualityAsync(maxQuality.Value);
+
+                amount++;
+            }
+
+            await PrintLocalizedAsync("Success", new { Amount = amount });
+        }
+    }
+}
---
+++ SilK.Unturned.Essentials/Commands/Items/CRepairHand.cs
@@ -0,0 +1,68 @@
+﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Prioritization;
+using OpenMod.Core.Commands;
+using OpenMod.Unturned.Items;
+using OpenMod.Unturned.Users;
+using SDG.Unturned;
+using SilK.Unturned.Essentials.Localization;
+using System;
+
+namespace SilK.Unturned.Essentials.Commands.Items
+{
+    [UsedImplicitly]
+    [Command("repairhand", Priority = Priority.Normal)]
+    [CommandAlias("fixhand")]
+    [CommandDescription("Repairs the item in your hand.")]
+    [CommandActor(typeof(UnturnedUser))]
+    [LocalizationSection("Items", "RepairHand")]
+    public class CRepairHand : EssentialsCommand
+    {
+        public CRepairHand(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        [UsedImplicitly]
+        protected async UniTask OnExecuteAsync()
+        {
+            await UniTask.SwitchToMainThread();
+
+            var (item, asset, page, x, y) = GetEquippedItem();
+            
+            if (item == null || asset == null)
+            {
+                throw GetLocalizedFriendlyException("NoEquipped");
+            }
+
+            UnturnedPlayer.Inventory.Inventory.sendUpdateQuality(page, x, y, asset.qualityMax);
+
+            var unturnedItemAsset = new UnturnedItemAsset(asset);
+
+            await PrintLocalizedAsync("Success", new { ItemAsset = unturnedItemAsset });
+        }
+
+        private (ItemJar? Item, ItemAsset? ItemAsset, byte Page, byte X, byte Y) GetEquippedItem()
+        {
+            var nativePlayer = UnturnedPlayer.Player;
+
+            var equipment = nativePlayer.equipment;
+
+            var asset = equipment.asset;
+            var page = equipment.equippedPage;
+            var x = equipment.equipped_x;
+            var y = equipment.equipped_y;
+
+            var index = nativePlayer.inventory.getIndex(page, x, y);
+
+            if (index == byte.MaxValue)
+            {
+                return (null, null, byte.MaxValue, byte.MaxValue, byte.MaxValue);
+            }
+
+            var item = nativePlayer.inventory.getItem(page, index);
+
+            return (item, asset, page, x, y);
+        }
+    }
+}
