0c475c96d624e2a9b7e1ad576653b725ac48669a
Stephen White <iamsilk3495@gmail.com>
2022-01-28 00:28:25 -0400

    Added helper method for getting LocalizationSection attributes and inherited attributes

---
+++ SilK.Unturned.Essentials/Localization/LocalizationSectionHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SilK.Unturned.Essentials.Localization
+{
+    public static class LocalizationSectionHelper
+    {
+        public static string[] GetSubSectionsFromType(Type type)
+        {
+            var inheritedTypes = new Stack<Type>();
+
+            var current = type;
+
+            while (current != null)
+            {
+                inheritedTypes.Push(current);
+                current = current.BaseType;
+            }
+
+            var subSections = new List<string>();
+
+            while (inheritedTypes.Peek() != null)
+            {
+                current = inheritedTypes.Pop();
+
+                var localizationSection = current.GetCustomAttribute<LocalizationSectionAttribute>();
+
+                if (localizationSection == null)
+                {
+                    continue;
+                }
+
+                subSections.AddRange(localizationSection.SectionNames);
+            }
+
+            return subSections.ToArray();
+        }
+    }
+}
