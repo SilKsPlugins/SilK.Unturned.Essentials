3381255479553217aeded1b0dc423f95e59c92ad
Stephen White <iamsilk3495@gmail.com>
2022-01-28 00:27:16 -0400

    Initial commit

---
+++ SilK.Unturned.Essentials/UnturnedEssentialsPlugin.cs
@@ -0,0 +1,15 @@
+﻿extern alias JetBrainsAnnotations;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.Unturned.Plugins;
+using System;
+
+namespace SilK.Unturned.Essentials
+{
+    [UsedImplicitly]
+    public class UnturnedEssentialsPlugin : OpenModUnturnedPlugin
+    {
+        public UnturnedEssentialsPlugin(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
