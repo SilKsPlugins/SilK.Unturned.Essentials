760e1dc063f0a8f5ff49b4469eb15dd33aee1ec7
Stephen White <iamsilk3495@gmail.com>
2022-01-28 00:28:51 -0400

    Added SubStringLocalizer for modularizing IStringLocalizers

---
+++ SilK.Unturned.Essentials/Localization/SubStringLocalizer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace SilK.Unturned.Essentials.Localization
+{
+    public class SubStringLocalizer : IStringLocalizer
+    {
+        public readonly IStringLocalizer BaseStringLocalizer;
+
+        public readonly string NamePrefix;
+
+        private readonly string[] _subSections;
+
+        public SubStringLocalizer(IStringLocalizer stringLocalizer, params string[] subSections)
+        {
+            BaseStringLocalizer = stringLocalizer;
+            NamePrefix = string.Join(":", subSections) + ":";
+            _subSections = subSections.ToArray();
+        }
+
+        public LocalizedString this[string name] => BaseStringLocalizer[NamePrefix + name];
+
+        public LocalizedString this[string name, params object[] arguments] => BaseStringLocalizer[NamePrefix + name, arguments];
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            return BaseStringLocalizer.GetAllStrings(includeParentCultures);
+        }
+
+        [Obsolete]
+        public IStringLocalizer WithCulture(CultureInfo culture)
+        {
+            return new SubStringLocalizer(BaseStringLocalizer.WithCulture(culture), _subSections);
+        }
+    }
+}
