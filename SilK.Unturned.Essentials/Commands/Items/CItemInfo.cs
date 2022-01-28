8ebfab85d5c266dcc3ae15b3a2dc2368eeb08f67
Stephen White <iamsilk3495@gmail.com>
2022-01-28 00:30:50 -0400

    Added /iteminfo command

---
+++ SilK.Unturned.Essentials/Commands/Items/CItemInfo.cs
@@ -0,0 +1,31 @@
+﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Prioritization;
+using OpenMod.Core.Commands;
+using OpenMod.Extensions.Games.Abstractions.Items;
+using OpenMod.Unturned.Items;
+using OpenMod.Unturned.Users;
+using SilK.Unturned.Essentials.Localization;
+using System;
+
+namespace SilK.Unturned.Essentials.Commands.Items
+{
+    [UsedImplicitly]
+    [Command("iteminfo", Priority = Priority.Normal)]
+    [CommandDescription("Shows info of an item.")]
+    [CommandActor(typeof(UnturnedUser))]
+    [LocalizationSection("Items", "ItemInfo")]
+    public class CItemInfo : EssentialsCommand
+    {
+        public CItemInfo(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        [UsedImplicitly]
+        protected async UniTask OnExecuteAsync(UnturnedItemAsset item)
+        {
+            await PrintLocalizedAsync("Success", new { ItemAsset = item });
+        }
+    }
+}
