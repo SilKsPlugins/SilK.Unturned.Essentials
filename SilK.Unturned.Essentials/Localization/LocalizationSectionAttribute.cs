4070b221f367e152715bfd1772f9525ed036397c
Stephen White <iamsilk3495@gmail.com>
2022-01-28 00:28:07 -0400

    Added LocalizationSection attribute for modularization of localization file's usage

---
+++ SilK.Unturned.Essentials/Localization/LocalizationSectionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SilK.Unturned.Essentials.Localization
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class LocalizationSectionAttribute : Attribute
+    {
+        public string[] SectionNames { get; set; }
+
+        public LocalizationSectionAttribute(params string[] sectionNames)
+        {
+            SectionNames = sectionNames;
+        }
+    }
+}
