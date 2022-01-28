a6b0229cac609584044134a9f4d7b727f3b52e02
Stephen White <iamsilk3495@gmail.com>
2022-01-28 01:35:50 -0400

    Add /refuelvehicle and /refuelvehicle all commands

---
+++ SilK.Unturned.Essentials/Commands/Vehicles/CRefuelVehicle.cs
@@ -0,0 +1,66 @@
+﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Prioritization;
+using OpenMod.Core.Commands;
+using OpenMod.Unturned.Users;
+using OpenMod.Unturned.Vehicles;
+using SDG.Unturned;
+using SilK.Unturned.Essentials.Localization;
+using System;
+using UnityEngine;
+
+namespace SilK.Unturned.Essentials.Commands.Vehicles
+{
+    [UsedImplicitly]
+    [Command("refuelvehicle", Priority = Priority.Normal)]
+    [CommandAlias("refuel")]
+    [CommandSyntax("[all]")]
+    [CommandDescription("Refuels the vehicle you are in or all vehicles.")]
+    [CommandActor(typeof(UnturnedUser))]
+    [LocalizationSection("Vehicles", "RefuelVehicle")]
+    public class CRefuelVehicle : EssentialsCommand
+    {
+        public CRefuelVehicle(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        [UsedImplicitly]
+        protected async UniTask OnExecuteAsync()
+        {
+            await UniTask.SwitchToMainThread();
+
+            var vehicle = UnturnedPlayer.CurrentVehicle;
+
+            vehicle ??= GetVehicleFromLook();
+
+            if (vehicle == null)
+            {
+                throw GetLocalizedFriendlyException("NoVehicle");
+            }
+
+            vehicle.Vehicle.askFillFuel(ushort.MaxValue);
+
+            await PrintLocalizedAsync("Success", new { Vehicle = vehicle });
+        }
+
+        private UnturnedVehicle? GetVehicleFromLook()
+        {
+            const float maxVehicleDistance = 8;
+
+            var nativePlayer = UnturnedPlayer.Player;
+
+            Physics.Raycast(nativePlayer.look.aim.position, nativePlayer.look.aim.forward, out var raycastHit, maxVehicleDistance,
+                RayMasks.VEHICLE);
+
+            if (raycastHit.transform == null)
+            {
+                return null;
+            }
+
+            var vehicle = raycastHit.transform.GetComponent<InteractableVehicle>();
+
+            return vehicle == null ? null : new UnturnedVehicle(vehicle);
+        }
+    }
+}
---
+++ SilK.Unturned.Essentials/Commands/Vehicles/CRefuelVehicleAll.cs
@@ -0,0 +1,48 @@
+﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Prioritization;
+using OpenMod.Core.Commands;
+using OpenMod.Extensions.Games.Abstractions.Vehicles;
+using OpenMod.Unturned.Users;
+using OpenMod.Unturned.Vehicles;
+using SilK.Unturned.Essentials.Localization;
+using System;
+using System.Linq;
+
+namespace SilK.Unturned.Essentials.Commands.Vehicles
+{
+    [UsedImplicitly]
+    [CommandParent(typeof(CRefuelVehicle))]
+    [Command("all", Priority = Priority.Normal)]
+    [CommandDescription("Refuels all vehicles.")]
+    [CommandActor(typeof(UnturnedUser))]
+    [LocalizationSection("Vehicles", "RefuelVehicleAll")]
+    public class CRefuelVehicleAll : EssentialsCommand
+    {
+        private readonly IVehicleDirectory _vehicleDirectory;
+
+        public CRefuelVehicleAll(IServiceProvider serviceProvider,
+            IVehicleDirectory vehicleDirectory) : base(serviceProvider)
+        {
+            _vehicleDirectory = vehicleDirectory;
+        }
+
+        [UsedImplicitly]
+        protected async UniTask OnExecuteAsync()
+        {
+            await UniTask.SwitchToMainThread();
+
+            var vehicles = await _vehicleDirectory.GetVehiclesAsync();
+
+            await UniTask.SwitchToMainThread();
+
+            foreach (var vehicle in vehicles.OfType<UnturnedVehicle>())
+            {
+                vehicle.Vehicle.askFillFuel(ushort.MaxValue);
+            }
+
+            await PrintLocalizedAsync("Success", new { Amount = vehicles.Count });
+        }
+    }
+}
