68da5cb6efcd596fa1e7f2548c4d85e087c9f69f
Stephen White <iamsilk3495@gmail.com>
2022-01-28 00:29:19 -0400

    Added command parameter resolve provider for item assets

---
+++ SilK.Unturned.Essentials/Commands/Items/UnturnedItemAssetCommandParameterResolveProvider.cs
@@ -0,0 +1,64 @@
+﻿using OpenMod.API.Commands;
+using OpenMod.Extensions.Games.Abstractions.Items;
+using OpenMod.Unturned.Items;
+using SDG.Unturned;
+using System;
+using System.Threading.Tasks;
+
+namespace SilK.Unturned.Essentials.Commands.Items
+{
+    public class UnturnedItemAssetCommandParameterResolveProvider : ICommandParameterResolveProvider
+    {
+        private readonly IItemDirectory _itemDirectory;
+
+        public UnturnedItemAssetCommandParameterResolveProvider(IItemDirectory itemDirectory)
+        {
+            _itemDirectory = itemDirectory;
+        }
+
+        public bool Supports(Type type)
+        {
+            return type == typeof(UnturnedItemAsset) || type == typeof(IItemAsset) || type == typeof(ItemAsset);
+        }
+
+        public async Task<object?> ResolveAsync(Type type, string input)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (!Supports(type))
+            {
+                throw new ArgumentException("The given type is not supported", nameof(type));
+            }
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+
+            var itemAsset = await _itemDirectory.FindByIdAsync(input) ??
+                            await _itemDirectory.FindByNameAsync(input, false);
+
+            if (type == typeof(IItemAsset))
+            {
+                return itemAsset;
+            }
+
+            var unturnedItemAsset = itemAsset as UnturnedItemAsset;
+
+            if (type == typeof(UnturnedItemAsset))
+            {
+                return unturnedItemAsset;
+            }
+
+            if (type == typeof(ItemAsset))
+            {
+                return unturnedItemAsset?.ItemAsset;
+            }
+
+            throw new InvalidOperationException($"Unable to return type {type}");
+        }
+    }
+}
---
+++ SilK.Unturned.Essentials/ServiceConfigurator.cs
@@ -0,0 +1,22 @@
+﻿extern alias JetBrainsAnnotations;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using OpenMod.API.Ioc;
+using OpenMod.Core.Commands;
+using SilK.Unturned.Essentials.Commands.Items;
+
+namespace SilK.Unturned.Essentials
+{
+    [UsedImplicitly]
+    public class ServiceConfigurator : IServiceConfigurator
+    {
+        public void ConfigureServices(IOpenModServiceConfigurationContext openModStartupContext,
+            IServiceCollection serviceCollection)
+        {
+            serviceCollection.Configure<CommandParameterResolverOptions>(options =>
+            {
+                options.AddCommandParameterResolveProvider<UnturnedItemAssetCommandParameterResolveProvider>();
+            });
+        }
+    }
+}
