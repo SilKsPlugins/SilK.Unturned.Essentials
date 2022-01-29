ab26fd555d877d1debe84cef33ae072f6423bcb3
IAmSilK <iamsilk3495@gmail.com>
2022-01-29 01:43:12 -0400

    Add StringHelper with FindBestMatch method

---
+++ SilK.Unturned.Essentials/Helpers/StringHelper.cs
@@ -0,0 +1,22 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SilK.Unturned.Essentials.Helpers
+{
+    public static class StringHelper
+    {
+        public static T? FindBestMatch<T>(this IEnumerable<T> enumerable, string searchTerm, Func<T, string> searchTermFunc)
+        {
+            searchTerm = searchTerm.ToLower();
+
+            return enumerable.Where(element =>
+                    searchTermFunc(element).IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) >= 0)
+                .MinBy(element =>
+                    global::OpenMod.Core.Helpers.StringHelper.LevenshteinDistance(searchTerm,
+                        searchTermFunc(element).ToLower()))
+                .FirstOrDefault();
+        }
+    }
+}
