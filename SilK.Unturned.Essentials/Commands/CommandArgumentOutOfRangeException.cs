7a4c90f4249becc78cbdea4956451050e64dc9cd
IAmSilK <iamsilk3495@gmail.com>
2022-01-29 01:36:50 -0400

    Add CommandArgumentOutOfRangeException

---
+++ SilK.Unturned.Essentials/Commands/CommandArgumentOutOfRangeException.cs
@@ -0,0 +1,11 @@
+﻿using OpenMod.API.Commands;
+
+namespace SilK.Unturned.Essentials.Commands
+{
+    public class CommandArgumentOutOfRangeException : UserFriendlyException
+    {
+        public CommandArgumentOutOfRangeException(string message) : base(message)
+        {
+        }
+    }
+}
