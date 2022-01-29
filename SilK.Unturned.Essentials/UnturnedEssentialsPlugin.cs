09c47bcd9fc07d438b8c5f1f1573da11940fff20
IAmSilK <iamsilk3495@gmail.com>
2022-01-29 01:34:42 -0400

    Add plugin metadata

--- SilK.Unturned.Essentials/UnturnedEssentialsPlugin.cs
+++ SilK.Unturned.Essentials/UnturnedEssentialsPlugin.cs
@@ -1,7 +1,10 @@
 ﻿extern alias JetBrainsAnnotations;
 using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Plugins;
 using OpenMod.Unturned.Plugins;
 using System;
+
+[assembly: PluginMetadata("SilK.Unturned.Essentials", DisplayName = "SilK's Unturned Essentials", Author = "SilK")]
 
 namespace SilK.Unturned.Essentials
 {
