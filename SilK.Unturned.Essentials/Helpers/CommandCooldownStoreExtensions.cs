35bd92dfc0d73546ebbaae0cba2490994309d97d
IAmSilK <iamsilk3495@gmail.com>
2022-01-29 01:46:26 -0400

    Add extension for ICommandCooldownStore

---
+++ SilK.Unturned.Essentials/Helpers/CommandCooldownStoreExtensions.cs
@@ -0,0 +1,20 @@
+﻿using OpenMod.API.Commands;
+using OpenMod.Core.Cooldowns;
+using System;
+using System.Threading.Tasks;
+
+namespace SilK.Unturned.Essentials.Helpers
+{
+    public static class CommandCooldownStoreExtensions
+    {
+        public static async Task<TimeSpan> GetTimeLeft(this ICommandCooldownStore cooldownStore, ICommandActor actor,
+            string cooldownId, TimeSpan cooldown)
+        {
+            var lastExecuted = await cooldownStore.GetLastExecutedAsync(actor, cooldownId) ?? DateTime.MinValue;
+
+            var timeLeft = cooldown - (DateTime.Now - lastExecuted);
+
+            return timeLeft;
+        }
+    }
+}
