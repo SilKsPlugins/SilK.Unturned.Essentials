c96ca43088193744a1c1511976c489229a293546
IAmSilK <iamsilk3495@gmail.com>
2022-01-29 01:43:39 -0400

    Add EsentialsDataStoreWarpLocationSource to plugin lifetime scope

---
+++ SilK.Unturned.Essentials/IoC/PluginContainerConfigurator.cs
@@ -0,0 +1,18 @@
+﻿extern alias JetBrainsAnnotations;
+using Autofac;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Plugins;
+using SilK.Unturned.Essentials.Warps.Source;
+
+namespace SilK.Unturned.Essentials.IoC
+{
+    [UsedImplicitly]
+    public class PluginContainerConfigurator : IPluginContainerConfigurator
+    {
+        public void ConfigureContainer(IPluginServiceConfigurationContext context)
+        {
+            context.ContainerBuilder.RegisterType<EssentialsDataStoreWarpLocationSource>()
+                .SingleInstance();
+        }
+    }
+}
