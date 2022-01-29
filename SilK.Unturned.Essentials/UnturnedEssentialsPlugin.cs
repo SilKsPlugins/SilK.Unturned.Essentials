70fad5d24cae321f557666296e837d22ed7dcd46
IAmSilK <iamsilk3495@gmail.com>
2022-01-29 01:45:27 -0400

    Create and load/unload plugin component manager on plugin load/unload

--- SilK.Unturned.Essentials/UnturnedEssentialsPlugin.cs
+++ SilK.Unturned.Essentials/UnturnedEssentialsPlugin.cs
@@ -1,7 +1,9 @@
 ﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
 using JetBrainsAnnotations::JetBrains.Annotations;
 using OpenMod.API.Plugins;
 using OpenMod.Unturned.Plugins;
+using SilK.Unturned.Essentials.Components;
 using System;
 
 [assembly: PluginMetadata("SilK.Unturned.Essentials", DisplayName = "SilK's Unturned Essentials", Author = "SilK")]
@@ -11,8 +13,21 @@ namespace SilK.Unturned.Essentials
     [UsedImplicitly]
     public class UnturnedEssentialsPlugin : OpenModUnturnedPlugin
     {
+        private readonly PluginComponentManager _pluginComponentManager;
+
         public UnturnedEssentialsPlugin(IServiceProvider serviceProvider) : base(serviceProvider)
         {
+            _pluginComponentManager = new PluginComponentManager(this);
+        }
+        protected override async UniTask OnLoadAsync()
+        {
+
+            await _pluginComponentManager.LoadAsync();
+        }
+
+        protected override async UniTask OnUnloadAsync()
+        {
+            await _pluginComponentManager.UnloadAsync();
         }
     }
 }
