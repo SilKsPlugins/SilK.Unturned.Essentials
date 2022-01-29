d98878ccae48ef2d03062576983e8d01d5ff0339
IAmSilK <iamsilk3495@gmail.com>
2022-01-29 01:46:40 -0400

    Add /warp and /warp add commands

---
+++ SilK.Unturned.Essentials/Commands/Warps/CWarp.cs
@@ -0,0 +1,63 @@
+﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Prioritization;
+using OpenMod.Core.Commands;
+using OpenMod.Core.Cooldowns;
+using OpenMod.Unturned.Users;
+using SilK.Unturned.Essentials.API.Warps;
+using SilK.Unturned.Essentials.Helpers;
+using SilK.Unturned.Essentials.Localization;
+using System;
+
+namespace SilK.Unturned.Essentials.Commands.Warps
+{
+    [UsedImplicitly]
+    [Command("warp", Priority = Priority.Normal)]
+    [CommandSyntax("<warp name>")]
+    [CommandDescription("Teleport to a warp.")]
+    [CommandActor(typeof(UnturnedUser))]
+    [LocalizationSection("Warps", "Warp")]
+    public class CWarp : EssentialsCommand
+    {
+        private readonly ICommandCooldownStore _cooldownStore;
+        
+        public CWarp(IServiceProvider serviceProvider,
+            ICommandCooldownStore cooldownStore) : base(serviceProvider)
+        {
+            _cooldownStore = cooldownStore;
+        }
+
+        [UsedImplicitly]
+        protected async UniTask OnExecuteAsync(IWarpLocation warp)
+        {
+            var cooldownId = GetWarpCooldownId(warp);
+
+            if (warp.Cooldown > TimeSpan.Zero)
+            {
+                var timeLeft = await _cooldownStore.GetTimeLeft(Context.Actor, cooldownId, warp.Cooldown);
+
+                if (timeLeft > TimeSpan.Zero)
+                {
+                    throw GetLocalizedFriendlyException("Cooldown", new { Warp = warp, TimeLeft = timeLeft });
+                }
+            }
+
+            if (await UnturnedPlayer.SetPositionAsync(warp.Position, warp.Rotation))
+            {
+                await _cooldownStore.RecordExecutionAsync(Context.Actor, cooldownId, DateTime.Now);
+
+                await PrintLocalizedAsync("Success", new { Warp = warp });
+            }
+            else
+            {
+                await PrintLocalizedAsync("Failure", new { Warp = warp });
+            }
+        }
+
+        private string GetWarpCooldownId(IWarpLocation warp)
+        {
+            return $"{Plugin.OpenModComponentId}:warps.{warp.Name}";
+        }
+    }
+}
---
+++ SilK.Unturned.Essentials/Commands/Warps/CWarpAdd.cs
@@ -0,0 +1,50 @@
+﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using OpenMod.API.Prioritization;
+using OpenMod.Core.Commands;
+using OpenMod.Unturned.Users;
+using SilK.Unturned.Essentials.Localization;
+using SilK.Unturned.Essentials.Warps.Source;
+using System;
+
+namespace SilK.Unturned.Essentials.Commands.Warps
+{
+    [UsedImplicitly]
+    [CommandParent(typeof(CWarp))]
+    [Command("add", Priority = Priority.Normal)]
+    [CommandSyntax("<warp name> [cooldown]")]
+    [CommandDescription("Add a new warp location.")]
+    [CommandActor(typeof(UnturnedUser))]
+    [LocalizationSection("Warps", "WarpAdd")]
+    public class CWarpAdd : EssentialsCommand
+    {
+        private readonly EssentialsDataStoreWarpLocationSource _warpLocationSource;
+
+        public CWarpAdd(IServiceProvider serviceProvider,
+            EssentialsDataStoreWarpLocationSource warpLocationSource) : base(serviceProvider)
+        {
+            _warpLocationSource = warpLocationSource;
+        }
+
+        [UsedImplicitly]
+        protected async UniTask OnExecuteAsync(string warpName, TimeSpan cooldown = default)
+        {
+            warpName = warpName.ToLower();
+
+            var position = UnturnedPlayer.Transform.Position;
+            var rotation = UnturnedPlayer.Transform.Rotation;
+
+            var added = await _warpLocationSource.AddOrUpdateAsync(warpName, data =>
+            {
+                data.Name = warpName;
+                data.Position = position;
+                data.Rotation = rotation;
+                data.Cooldown = cooldown;
+            });
+
+            await PrintLocalizedAsync(added ? "SuccessAdded" : "SuccessUpdated",
+                new { WarpName = warpName, Cooldown = cooldown });
+        }
+    }
+}
